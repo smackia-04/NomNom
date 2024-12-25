9429797251d1fd2f400eb6d97408cf2d45fb36c6
smackia-04 <anshbhargava42@gmail.com>
2024-12-25 15:41:40 +0530

    Just do it.

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+const myFirstElement = <h1>This is my first heading.</h1>;n
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(myFirstElement);
