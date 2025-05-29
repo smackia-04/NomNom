cbd2b675019c0c51e387647d4a91409b81183c77
smackia-04 <anshbhargava42@gmail.com>
2025-05-29 13:56:55 +0530

    Updated APIs

--- src/utils.js/constants.js
+++ src/utils.js/constants.js
@@ -1,7 +1,7 @@
 export const IMG_CDN_LINK = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
 
-export const FETCH_RESTAURANTS_URL = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.2814092&lng=82.99786259999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+export const FETCH_RESTAURANTS_URL = "https://foodfire.onrender.com/api/restaurants?lat=25.2814092&lng=82.99786259999999&page_type=DESKTOP_WEB_LISTING"
 
-export const FETCH_MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=25.2814092&lng=82.99786259999999&restaurantId="
+export const FETCH_MENU_URL = "https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=25.2814092&lng=82.99786259999999&restaurantId="
 
 export const MENU_ITEM_CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/";
